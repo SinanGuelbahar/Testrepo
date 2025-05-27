c892bb72c25b024233476f56e06ee9ac373b2456
Alexander Barwig <alexander.barwig87@gmail.com>
2025-05-27 13:27:33 +0200

    Update Aufgabe.py
    passt scho

--- Aufgabe.py
+++ Aufgabe.py
@@ -2,3 +2,4 @@ print("Hallo Klasse")
 print("Die Aufagbe ist das jeder eine Zeile hier " \
 "hinzufügen kann wenn es Konflikte git soll es passend gelöst werden!!! " \
 "ein sogenannter merge konflikt")
+print("Hallo, bitte nicht wieder löschen ich bin schreibfaul. danke =)")
