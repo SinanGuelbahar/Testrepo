28874b98da8d9a7570ac76d146c58166cc8cba01
Andreas Koch <dronfoto@googlemail.com>
2025-05-27 13:07:22 +0200

    hh

--- Aufgabe.py
+++ Aufgabe.py
@@ -4,9 +4,8 @@ print("Die Aufagbe ist das jeder eine Zeile hier " \
 "ein sogenannter merge konflikt")
 print("Hallo Hallo, test 123")
 print("Sinan")
-print("Hello")
-<<<<<<< HEAD
+print("Hello");
 print("Hallo, ich bin der neue Schüler")
-=======
+
 print("Hi")
->>>>>>> be40bd09097fc32c83dfe3ef0e66a0db28af1dcb
+
