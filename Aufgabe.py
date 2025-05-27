316f4c9ad7a7ef5c33a0f4caeeabcc5907f45b0b
SinanGuelbahar <sinan.guelbahar@bfw-nuernberg.de>
2025-05-27 13:29:02 +0200

    Update Aufgabe.py

--- Aufgabe.py
+++ Aufgabe.py
@@ -3,3 +3,4 @@ print("Die Aufagbe ist das jeder eine Zeile hier " \
 "hinzufügen kann wenn es Konflikte git soll es passend gelöst werden!!! " \
 "ein sogenannter merge konflikt")
 print("Hallo, bitte nicht wieder löschen ich bin schreibfaul. danke =)")
+print("13:28")
