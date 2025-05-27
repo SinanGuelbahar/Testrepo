cda0ad5a46b1055606e65b966497db69a720402f
SinanGuelbahar <sinan.guelbahar@bfw-nuernberg.de>
2025-05-27 12:24:26 +0200

    aufagbenstellung

--- Aufgabe.py
+++ Aufgabe.py
@@ -1,1 +1,4 @@
 print("Hallo Klasse")
+print("Die Aufagbe ist das jeder eine Zeile hier " \
+"hinzufügen kann wenn es Konflikte git soll es passend gelöst werden!!! " \
+"ein sogenannter merge konflikt")
